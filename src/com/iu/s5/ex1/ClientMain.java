5133b24cfe0c080c4fd0a540e0388013cb186b0e
jerry <woffl_@naver.com>
2022-01-17 17:53:29 +0900

    sd

--- src/com/iu/s5/ex1/ClientMain.java
+++ src/com/iu/s5/ex1/ClientMain.java
@@ -1,7 +1,10 @@
 package com.iu.s5.ex1;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
@@ -15,10 +18,10 @@ public class ClientMain {
 		Socket socket = null;
 		Scanner sc = new Scanner(System.in);
 		try {
-			socket = new Socket("192.168.35.177", 8283);
+			socket = new Socket("192.168.35.177", 8282);
 			System.out.println("서버와 접속 성공!");
 			System.out.println("서버로 보낼 메세지 입력");
-			String message = sc.next();
+			String message = sc.nextLine();
 			System.out.println(message); // hello -> h, e, l ,l, o -> 01
 			
 			//1. output stream을 구해오기
@@ -31,6 +34,13 @@ public class ClientMain {
 			bw.write(message+"\r\n");
 			//강제로 버퍼 비우기
 			bw.flush();
+			
+			InputStream is = socket.getInputStream(); //01 처리
+			InputStreamReader ir = new InputStreamReader(is); //char
+			BufferedReader br = new BufferedReader(ir); //String
+			
+			message = br.readLine();
+			System.out.println("Client : "+message);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/com/iu/s5/ex1/ServerMain.java
+++ src/com/iu/s5/ex1/ServerMain.java
@@ -1,10 +1,14 @@
 package com.iu.s5.ex1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ServerMain {
 
@@ -12,9 +16,10 @@ public class ServerMain {
 		System.out.println("Server 입니다");
 		ServerSocket serverSocket = null;
 		Socket socket = null;
+		Scanner sc = new Scanner(System.in);
 		try {
 			System.out.println("클라이언트 접속을 기다리는 중");
-			serverSocket = new ServerSocket(8283);
+			serverSocket = new ServerSocket(8282);
 			socket = serverSocket.accept();
 			System.out.println("접속한 cilent와 1:1 연결 완료");
 			
@@ -25,6 +30,18 @@ public class ServerMain {
 			
 			String message = br.readLine();
 			System.out.println("Client : "+message);
+			
+			System.out.println("Cilent로 전송할 메세지 입력");
+			message = sc.nextLine();
+			//server에서 클라이언트로 메세지 전송
+			OutputStream os = socket.getOutputStream();
+			OutputStreamWriter ow = new OutputStreamWriter(os);
+			BufferedWriter bw = new BufferedWriter(ow);
+			
+			//실제 전송
+			bw.write(message+"\r\n");
+			//강제로 버퍼 비우기
+			bw.flush();
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
